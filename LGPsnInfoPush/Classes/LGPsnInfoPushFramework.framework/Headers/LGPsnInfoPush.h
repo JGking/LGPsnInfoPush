691d530400b473174514a9da7b38f58107527ed6
JieGuan <610314675@qq.com>
2019-12-24 18:32:21 +0800

    update

--- LGPsnInfoPush/Classes/LGPsnInfoPushFramework.framework/Headers/LGPsnInfoPush.h
+++ LGPsnInfoPush/Classes/LGPsnInfoPushFramework.framework/Headers/LGPsnInfoPush.h
@@ -10,10 +10,27 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+
+@protocol LGPsnInfoPushDelegate <NSObject>
+
+@optional
+
+
+//Socket开启了长链接
+
+- (void)LGPsnSocketDidOpen;
+
+//Socket关闭了长链接
+- (void)LGPsnSocketDidClose;
+
+
+@end
+
 @interface LGPsnInfoPush : NSObject
 
 + (LGPsnInfoPush *)defaultManager;
 
+@property (nonatomic, weak) id<LGPsnInfoPushDelegate> LGPushDelegate;
 
 /** 基础平台地址 */
 @property (nonatomic, copy) NSString *BaseURL;
@@ -28,8 +45,15 @@ NS_ASSUME_NONNULL_BEGIN
 /** 极光推送返回的设备ID */
 @property (nonatomic, copy) NSString *JpushID;
 
+/** 是否需要推送消息 */
+@property (nonatomic,assign) BOOL  IsNeedPush;
+
+
+
 - (void)closeSocket;//关闭连接
 - (void)openSocket;//开启连接
+
+
 
 @end
 
--- LGPsnInfoPush/Classes/LGPsnInfoPushFramework.framework/Headers/LGPsnInfoPushFramework.h
+++ LGPsnInfoPush/Classes/LGPsnInfoPushFramework.framework/Headers/LGPsnInfoPushFramework.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char LGPsnInfoPushFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LGPsnInfoPushFramework/PublicHeader.h>
 
-#import "LGPsnInfoPush.h"
+
